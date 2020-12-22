c0babe2c1e6b4a68e00ffb75b1b9f2bff4bf3414
PaulSchulteSythen <paulsythen@gmail.com>
2020-12-22 19:42:25 +0100

    first commit

---
+++ Speech.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+#print(sr.__version__)
+
+# create instance of Recognizer
+recognizer = sr.Recognizer()
+
+# record signal
+mic = sr.Microphone()
+while(True):
+	try:
+		print("Say something!")
+		with mic as source:
+			# get audio from microphone
+			audio = recognizer.listen(source)
+			
+			# extract text
+			text = recognizer.recognize_google(audio)
+			text = text.lower()
+			if "return" in text:
+				break
+			elif "you" in text:
+				print("I'm fine")
+			else:
+				print(text)
+	except KeyboardInterrupt:
+		break
+	except:
+		print("Error")
